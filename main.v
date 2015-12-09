6478b56e545308a11d4edf23f72069a86fd9586a
pinethee@lime <pinethee@naver.com>
2015-12-09 21:48:34 +0900

    revised main

--- main.v
+++ main.v
@@ -1,5 +1,5 @@
 module main(
-	input sw2_, input sw3_, input sw4_, input sw5_, input sw6_, input sw7_,
+	input[5:0]sw_,
 	input[9:0] dipSwitch,
 	input clk,
 
@@ -15,22 +15,34 @@ module main(
 );
 
 // **** SETUP **** //
-wire sw2, sw3, sw4, sw5, sw6, sw7;
-ButtonDebouncer btn_db1(clk, sw2_, sw2); // FIXME: 모듈명 및 인자 전달 순서 바뀔 수 있음
-ButtonDebouncer btn_db2(clk, sw2_, sw3);
-ButtonDebouncer btn_db3(clk, sw2_, sw4);
-ButtonDebouncer btn_db4(clk, sw2_, sw5);
-ButtonDebouncer btn_db5(clk, sw2_, sw6);
-ButtonDebouncer btn_db6(clk, sw2_, sw7);
+wire [5:0]sw;
+ButtonDebouncer btn_db1(clk, sw_[0], sw[0]); // FIXME: 모듈명 및 인자 전달 순서 바뀔 수 있음
+ButtonDebouncer btn_db2(clk, sw_[1], sw[1]);
+ButtonDebouncer btn_db3(clk, sw_[2], sw[2]);
+ButtonDebouncer btn_db4(clk, sw_[3], sw[3]);
+ButtonDebouncer btn_db5(clk, sw_[4], sw[4]);
+ButtonDebouncer btn_db6(clk, sw_[5], sw[5]);
 
 
 // **** REGISTER **** //
-reg[4:0] hour; // 0 to 24
-reg[5:0] min, sec; // 0 to 60
+reg[4:0] hour = 0, alarm_hour = 0; // 0 to 24
+reg[5:0] min = 0, sec = 0, 
+	alarm_min = 0, alarm_sec = 0,
+	stop_min = 0, stop_sec = 0; // 0 to 60
+reg[6:0] stop_msec = 0; // 0 to 99
 
-Time_counter TimeCounter(clk, hour, min, sec);
+reg[??] state, nextState; // FIXME : 몇 개가 될 지 일단 보류
+reg[??] sign; // 알람 울리기 등의 event sign; FIXME : 몇 개 울릴지 일단 보류 
+// parameter[??] TIME24, TIME12, TIME_SETTING, ALARM_SETTING, STOPWATCH_SETTING;
+// parameter[??] ALARMING, RESET; // FIXME : sign 종류 추가 필요 
+
+Time_counter TimeCounter(clk, sw, hour, min, sec);
 
 // **** STATE MACHINE **** //
+TimeDisplayMode TD_mode(sw, state, hour, min, sec, nextState);
+TimeSettingMode TS_mode(sw, state, hour, min, sec, new_hour, new_min, new_sec, nextState);
+AlarmSettingMode AS_mode(sw, state, alarm_hour, alarm_min, nextState);
+StopwatchSettingMode SS_mode(sw, state, stop_min, stop_sec, stop_msec, nextState);
 
 
 
