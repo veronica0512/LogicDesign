df7b1e393916965263f09b92813b8a80d33c4d92
veronica0512 <veronica3465@gmail.com>
2015-12-13 19:58:24 +0900

    Update TimeCounter.v

--- TimeCounter.v
+++ TimeCounter.v
@@ -8,6 +8,12 @@ module TimeCounter(
     output sec_out
 );
 // hour/min/sec_in이 들어오면 그 시각에서부터 시간을 계산함
+wire minrci, hrci;
+
+	
+SecCounter sec(clk, sec_in, minrci, sec_out);
+MinCounter min(clk, minrci, min_in, hrci, min_out);
+HCounter hour(clk, hrci, hour_in, ,hour_out);
 
 
 endmodule
